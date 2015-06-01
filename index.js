c723d5c623847c93825ef70996344bb6247f7b55
Daniel Cousens <dcousens@users.noreply.github.com>
2015-06-01 14:59:51 +1000

    enforce no extra semis, consistent semi spacing

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ module.exports = {
   'extends': 'standard',
 
   'rules': {
-    'semi': [2, 'always']
+    'semi': [2, 'always'],
+    'no-extra-semi': 2,
+    'semi-spacing': [2, { 'before': false, 'after': true }]
   }
 }
